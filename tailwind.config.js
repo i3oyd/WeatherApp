2b736ddea44aeec1ad6709a338cb78b431aa0c8b
Jomel Julom <jajulom@gmail.com>
2022-08-13 02:11:45 +0800

    first commit: about the app

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      'weather-primary': 'rgb(110 231 183)',
+      'weather-secondary': '#004E71'
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif']
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
+  },
+  plugins: [],
+}
